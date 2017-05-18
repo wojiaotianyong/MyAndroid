f70fafb7c49fc03c30307d845da187a62639e661
tianyong <tianyong@clcw.com.cn>
2017-05-18 14:41:29 +0800

    upgrade

--- javatestlib/README.md
+++
@@ -1,4 +0,0 @@
-# MyAndroid
-The android technology
-
-参考 http://blog.csdn.net/xiaxiayige/article/details/46706949
--- javatestlib/src/main/java/com/example/TestClass.java
+++ javatestlib/src/main/java/com/example/TestClass.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class TestClass {
     public static void main(String [] args){
-        System.out.print("hello world2");
+        System.out.print("hello world1");
     }
 }
