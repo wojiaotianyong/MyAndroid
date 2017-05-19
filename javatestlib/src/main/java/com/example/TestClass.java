2047182a7a761a50079363d7db12b7ddb02bbdc2
tianyong <tianyong@clcw.com.cn>
2017-05-19 11:02:07 +0800

    upgrade

--- javatestlib/src/main/java/com/example/TestClass.java
+++ javatestlib/src/main/java/com/example/TestClass.java
@@ -1,7 +1,16 @@
 package com.example;
 
+import com.google.gson.JsonArray;
+
 public class TestClass {
-    public static void main(String [] args){
-        System.out.print("hello world1");
+    public static void main(String[] args) {
+//        System.out.print("hello world1");
+        JsonArray jsonArray = new JsonArray();
+        jsonArray.add("a");
+        jsonArray.add("b");
+        jsonArray.add("c");
+        for (int i = 0; i < jsonArray.size(); i++) {
+            System.out.println(jsonArray.get(i).getAsString());
+        }
     }
 }
